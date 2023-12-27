55cebc1a0f3f990d662b4995949bf9ed19283cac
ZahraAbdolalian <abdolalianzahra@gmail.com>
2023-12-27 14:11:07 +0330

    add page not found

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { TitleComponent } from '../shared/shared-component/title/title.component';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [],
+  imports: [
+    TitleComponent,
+    MatButtonModule,
+    RouterLink
+  ],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.scss'
 })

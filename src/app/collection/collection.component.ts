5151881ed34c0b4d3894187c75f73eb83b3cd04e
ZahraAbdolalian <abdolalianzahra@gmail.com>
2024-08-28 21:46:18 +0330

    add tailwind to collection page

--- src/app/collection/collection.component.ts
+++ src/app/collection/collection.component.ts
@@ -3,19 +3,17 @@ import { TitleComponent } from '../shared/shared-component/title/title.component
 import { CollectionFilterComponent } from '../shared/shared-component/collection-filter/collection-filter.component';
 import { CategoryFiltersComponent } from './category-filters/category-filters.component';
 import { NewProductComponent } from '../shared/shared-component/new-product/new-product.component';
-import { CollectionBannerComponent } from './collection-banner/collection-banner.component';
 import { ProductsCategorizationComponent } from './products-categorization/products-categorization.component';
 import { ScrollingService } from '../service/scrolling.service';
 
 @Component({
-  selector: 'app-collection',
+  selector: 'app-collection', 
   standalone: true,
   imports: [
     TitleComponent,
     CollectionFilterComponent,
     CategoryFiltersComponent,
     NewProductComponent,
-    CollectionBannerComponent,
     ProductsCategorizationComponent
   ],
   templateUrl: './collection.component.html',
